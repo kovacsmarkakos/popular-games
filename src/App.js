048e40fb4540fdd8272c64005827a9083aaaf453
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-10 18:32:01 +0200

    changed name, fetched data from api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react'
 import Cards from './components/Cards/Cards'
 import Filters from './components/Filters/Filters'
 import styles from './App.module.css'
+import axios from 'axios'
+import { fetchData } from './api'
 
-const App = () => {
-  return (
-    <div className={styles.container}>
-      <Filters />
-      <Cards />
-    </div>
-  )
+class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData()
+
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Filters />
+        <Cards />
+      </div>
+    )
+  }
 }
 
 export default App
