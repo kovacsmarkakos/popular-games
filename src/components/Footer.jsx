26dab08a8f36c798102e649668a1bea4fe1c73d8
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-17 14:11:38 +0200

    added social links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
+import { IconButton, Typography, Container, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://kovacsmarkakos.github.io/">
+        Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
-  );
+  )
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -25,11 +25,18 @@ const useStyles = makeStyles((theme) => ({
     minHeight: '15vh',
   },
   footer: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
   },
+  footerIcons: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  }
 }));
 
 const Footer = () => {
@@ -39,13 +46,29 @@ const Footer = () => {
     <div className={classes.root}>
       <CssBaseline />
       <footer className={classes.footer}>
-        <Container maxWidth="lg">
-          <Typography variant="body1">My sticky footer can be found here.</Typography>
+        <Container maxWidth="md">
+          <Typography variant="body1">Made by Mark Kovacs</Typography>
           <Copyright />
+        </Container>
+        <Container className={classes.footerIcons}>
+          <a href="https://www.linkedin.com/in/márk-kovács-0363b61b7/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <IconButton aria-label="github">
+              <LinkedInIcon />
+            </IconButton>
+          </a>
+          <a href="https://github.com/kovacsmarkakos"
+            target="_blank"
+            rel="noopener noreferrer">
+            <IconButton aria-label="linkedin">
+              <GitHubIcon />
+            </IconButton>
+          </a>
         </Container>
       </footer>
     </div>
-  );
+  )
 }
 
 export default Footer
