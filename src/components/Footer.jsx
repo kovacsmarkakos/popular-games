cc74f67cb20774ee7872cf00312d2b471524e231
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-18 20:26:42 +0200

    small fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
-import { IconButton, Typography, Container, Link } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import React from "react";
+import { IconButton, Typography, Container, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 function Copyright() {
   return (
     <Typography variant="body2">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://kovacsmarkakos.github.io/">
         Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
-  )
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '15vh',
+    display: "flex",
+    flexDirection: "column",
+    height: "15vh",
   },
   footer: {
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
     padding: theme.spacing(3, 2),
-    marginTop: 'auto',
+    marginTop: theme.spacing(2),
     backgroundColor: theme.palette.grey[900],
     color: theme.palette.grey[400],
-    '@media (min-width: 1280px)': {
-      padding: theme.spacing(3, 10)
+    "@media (min-width: 1280px)": {
+      padding: theme.spacing(3, 10),
     },
   },
   footerIcons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
+    display: "flex",
+    justifyContent: "flex-end",
   },
   iconButton: {
     color: theme.palette.grey[200],
-    '&:hover': {
+    "&:hover": {
       backgroundColor: theme.palette.grey[800],
-      color: '#f5da55',
+      color: "#f5da55",
     },
   },
 }));
@@ -59,24 +59,28 @@ const Footer = () => {
           <Copyright />
         </Container>
         <Container className={classes.footerIcons}>
-          <a href="https://www.linkedin.com/in/márk-kovács-0363b61b7/"
+          <a
+            href="https://www.linkedin.com/in/márk-kovács-0363b61b7/"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             <IconButton aria-label="github" className={classes.iconButton}>
               <LinkedInIcon />
             </IconButton>
           </a>
-          <a href="https://github.com/kovacsmarkakos"
+          <a
+            href="https://github.com/kovacsmarkakos"
             target="_blank"
-            rel="noopener noreferrer">
-            <IconButton aria-label="linkedin" className={classes.iconButton} >
+            rel="noopener noreferrer"
+          >
+            <IconButton aria-label="linkedin" className={classes.iconButton}>
               <GitHubIcon />
             </IconButton>
           </a>
         </Container>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
