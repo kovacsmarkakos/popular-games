15291f834ef343c9455aa77368ccfaa15147cbbd
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-19 14:06:14 +0200

    Updates

---
+++ precache-manifest.c98241ca7ac0b38f8890348a5daa3072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c5b5b5390f9859d26a7d850224c0d5",
+    "url": "/popular-games/index.html"
+  },
+  {
+    "revision": "d9c5e20d775cc556b0c3",
+    "url": "/popular-games/static/css/2.d48510fc.chunk.css"
+  },
+  {
+    "revision": "cd3f787373a64891163e",
+    "url": "/popular-games/static/css/main.45db3b2b.chunk.css"
+  },
+  {
+    "revision": "d9c5e20d775cc556b0c3",
+    "url": "/popular-games/static/js/2.664d9594.chunk.js"
+  },
+  {
+    "revision": "17be58b4f0edd308655e53d9f7b01f3d",
+    "url": "/popular-games/static/js/2.664d9594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3f787373a64891163e",
+    "url": "/popular-games/static/js/main.8ab796a7.chunk.js"
+  },
+  {
+    "revision": "4e841cc44cc320d0fc61",
+    "url": "/popular-games/static/js/runtime-main.4a58fa8d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/popular-games/precache-manifest.c98241ca7ac0b38f8890348a5daa3072.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/popular-games/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
