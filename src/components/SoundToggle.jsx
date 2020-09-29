1a914a2b35ddacc1a4541a975771c719d06fb91b
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-29 23:46:38 +0200

    cursor pointer fix

--- src/components/SoundToggle.jsx
+++ src/components/SoundToggle.jsx
@@ -5,6 +5,7 @@ import volume from "react-useanimations/lib/volume";
 const SoundToggle = ({ muted, setMuted }) => {
   return (
     <UseAnimation
+      style={{ cursor: "pointer" }}
       reverse={muted}
       onClick={() => {
         setMuted(!muted);
